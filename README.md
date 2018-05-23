e68c5d402f56204a4513ed1cfbd2f9d498d79a30
José González Krause <josef@hackercat.ninja>
2018-05-23 12:22:38 +0200

    Added all the stuff

--- README.md
+++ README.md
@@ -1,3 +1,12 @@
 # scfmt
 
-Shellcode formatter
+_Shellcode formatter_ is a tool for extracting the shellcode from an `objdump` output (tested on macho64 dump) and prints the output directly in a nice go copypasteable string.
+
+## Usage
+
+Simply pipe your `objdump` to `scfmt`:
+
+```bash
+> objdump -d test.macho64 | scfmt
+< var shellcode string = "\x48\x31\xc0\x48\x31\xff\x48\x31\xf6\x48\x31\xd2\xb8\x04\x00\x00\x02\xbf\x01\x00\x00\x00\x48\xbe\x00\x20\x00\x00\x00\x00\x00\x00\xba\x12\x00\x00\x00\x0f\x05\xb8\x01\x00\x00\x02\xbf\x00\x00\x00\x00\x0fx05"
+```
---
+++ scfmt.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"regexp"
+	"strings"
+)
+
+func main() {
+	var payar []string
+	scanner := bufio.NewScanner(os.Stdin)
+	re := regexp.MustCompile("([0-9a-f]{2}[[:space:]])")
+	isStart := false
+
+	for scanner.Scan() {
+		s := scanner.Text()
+		if !isStart {
+			if strings.HasPrefix(s, "start:") {
+				isStart = true
+			}
+
+			continue
+		}
+
+		if strings.HasPrefix(s, "    ") {
+			l1 := re.FindAllString(strings.TrimSpace(strings.Split(s, ":")[1]), -1)
+			payar = append(payar, l1...)
+		}
+	}
+
+	var ps string
+	fmt.Print("var shellcode string = \"")
+	for i, c := range payar {
+		if i == len(payar)-1 {
+			ps += fmt.Sprintf("x%s", strings.TrimSpace(c))
+		} else {
+			ps += fmt.Sprintf("\\x%s", strings.TrimSpace(c))
+		}
+
+	}
+	fmt.Printf("%s\"\n", ps)
+
+	if scanner.Err() != nil {
+		fmt.Println(scanner.Err())
+	}
+}
