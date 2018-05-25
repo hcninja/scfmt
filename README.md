f267934fa19c3ee969aa81ce431892a6ec1b0c6f
José González Krause <josef@hackercat.ninja>
2018-05-25 12:42:55 +0200

    Added support for converting a string in literal 64b hex values in little endian

--- README.md
+++ README.md
@@ -2,12 +2,22 @@
 
 _Shellcode formatter_ is a tool for extracting the shellcode from an `objdump` output (tested on macho64 dump) and prints the output directly in a nice go copypasteable string.
 
+It also supports some basic conversions for writing exploits.
+
 ## Usage
 
+### objdump to shellcode
 Simply pipe your `objdump` to `scfmt`:
 
 ```bash
-> objdump -d test.macho64 | scfmt
+> objdump -d test.macho64 | scfmt -e
 < // payload length: 51 (0x33)
 < var shellcode string = "\x48\x31\xc0\x48\x31\xff\x48\x31\xf6\x48\x31\xd2\xb8\x04\x00\x00\x02\xbf\x01\x00\x00\x00\x48\xbe\x00\x20\x00\x00\x00\x00\x00\x00\xba\x12\x00\x00\x00\x0f\x05\xb8\x01\x00\x00\x02\xbf\x00\x00\x00\x00\x0f\x05"
+```
+
+### String to little endian 64b hex
+
+```bash
+> scfmt -s '\//bin/sh'
+< 0x732f6e69622f2f
 ```
--- scfmt.go
+++ scfmt.go
@@ -2,13 +2,34 @@ package main
 
 import (
 	"bufio"
+	"flag"
 	"fmt"
+	"log"
 	"os"
 	"regexp"
 	"strings"
 )
 
 func main() {
+	escFlag := flag.Bool("e", false, "Builds the payload from an objdump")
+	slvFlag := flag.String("s", "", "Converts a string in literal 64b hex values in little endian")
+	flag.Parse()
+
+	log.SetFlags(0)
+
+	if *escFlag {
+		extractShellCode()
+		os.Exit(0)
+	} else if *slvFlag != "" {
+		stringLiteralValue(*slvFlag)
+	} else {
+		fmt.Println("Nothing to do…")
+		flag.PrintDefaults()
+	}
+}
+
+// extractShellCode takes the output of an objdump -d command and generates a payload usable by go
+func extractShellCode() {
 	var payar []string
 	scanner := bufio.NewScanner(os.Stdin)
 	re := regexp.MustCompile("([0-9a-f]{2}[[:space:]])")
@@ -46,4 +67,24 @@ func main() {
 	if scanner.Err() != nil {
 		fmt.Println(scanner.Err())
 	}
+}
+
+func stringLiteralValue(s string) {
+	var revByteSlice []byte
+
+	for i := len(s) - 1; i > 0; i-- {
+		revByteSlice = append(revByteSlice, byte(s[i]))
+	}
+
+	for i, b := range revByteSlice {
+		if i%8 == 0 && i < len(revByteSlice) {
+			fmt.Print("0x")
+		} else if i%8 == 7 && i < len(revByteSlice) {
+			fmt.Printf("%x\n", b)
+		} else {
+			fmt.Printf("%x", b)
+		}
+	}
+
+	fmt.Println("")
 }
